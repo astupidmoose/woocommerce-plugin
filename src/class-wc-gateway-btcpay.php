7240ee4b0931a195db16d986a2f1c0c6065b1307
Mike Olthoff <mike@stufftech.io>
2019-04-07 00:05:19 -0700

    Try changing for LTC

--- src/class-wc-gateway-btcpay.php
+++ src/class-wc-gateway-btcpay.php
@@ -1,14 +1,14 @@
 <?php
 
 /*
-    Plugin Name: BTCPay for WooCommerce
+    Plugin Name: BTCPay (LTC) for WooCommerce
     Plugin URI:  https://wordpress.org/plugins/btcpay-for-woocommerce
-    Description: Enable your WooCommerce store to accept Bitcoin with BTCPay.
+    Description: Enable your WooCommerce store to accept Litecoin with BTCPay.
     Author:      BTCPay
     Text Domain: BTCPay
     Author URI:  https://github.com/btcpayserver
 
-    Version:           3.0.6
+    Version:           3.0.7
     License:           Copyright 2011-2018 BTCPay & BitPay Inc., MIT License
     License URI:       https://github.com/btcpayserver/woocommerce-plugin/blob/master/LICENSE
     GitHub Plugin URI: https://github.com/btcpayserver/woocommerce-plugin
